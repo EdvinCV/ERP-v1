107cdb50d15e50291f157477faccbfe5e3c495d6
Ubuntu <ubuntu@ip-172-31-35-79.us-east-2.compute.internal>
2019-10-19 18:26:52 +0000

    Arreglado

--- routes/web.php
+++ routes/web.php
@@ -124,6 +124,6 @@ Route::get('/ventas/productoGanancias','VentasController@productoMasGanancia');
 Route::get('/ventas/productoMasVendido','VentasController@productoMasVendido');
 Route::get('/ventas/productoMenosVendido','VentasController@productoMenosVendido');
 
-Route::get(uri,'/test', function(){
+Route::get('/test', function(){
     return 'HOLA';
 });
